e0e064d3112702ecf72b1fc7ab7f8340fd77c022
Theo Mauhin <theo.mauhin@etu.univ-smb.fr>
2025-12-10 15:39:10 +0100

    Mise en place des UC et buttons qui changent les pages

--- Jeu_Poisson/MainWindow.xaml.cs
+++ Jeu_Poisson/MainWindow.xaml.cs
@@ -19,6 +19,50 @@ namespace Jeu_Poisson
         public MainWindow()
         {
             InitializeComponent();
+            AfficheAcceuil();
         }
+
+        public void AfficheAcceuil() //affiche la page d'acceuil
+        {
+            UCacceuil uc = new UCacceuil();
+            ZoneJeu.Content = uc;
+
+            uc.butJouer.Click += AfficheChoixSkin; //quand on clique sur le bouton jouer, on affiche le choix des skins
+
+            uc.butDiff.Click += AfficheChoixDifficulte; //quand on clique sur le bouton difficulté, on affiche le choix des difficultés
+
+            uc.butBoutique.Click += AfficheBoutique; //quand on clique sur le bouton boutique, on affiche la boutique
+
+            uc.butPara.Click += AfficheParametres; //quand on clique sur le bouton paramètres, on affiche les paramètres
+
+            uc.butQuit.Click += Fermer(); //quand on clique sur le bouton quitter, on ferme l'application
+        }
+        private void AfficheChoixSkin(object sender, RoutedEventArgs e) //affiche la page de choix des skins
+        {
+            UCChoixSkin uc = new UCChoixSkin();
+            ZoneJeu.Content = uc;
+        }
+        private void AfficheChoixDifficulte(object sender, RoutedEventArgs e) //affiche la page de choix des difficultés
+        {
+            UCDifficultés uc = new UCDifficultés();
+            ZoneJeu.Content = uc;
+        }
+        private void AfficheBoutique(object sender, RoutedEventArgs e) //affiche la page de la boutique
+        {
+            UCBoutique uc = new UCBoutique();
+            ZoneJeu.Content = uc;
+        }
+        private void AfficheParametres(object sender, RoutedEventArgs e) //affiche la page des paramètres
+        {
+            UCParamètres uc = new UCParamètres();
+            ZoneJeu.Content = uc;
+        }
+        
+        private RoutedEventHandler Fermer() //ferme l'application
+        {
+            return (sender, e) => { Application.Current.Shutdown(); }; //trouver la signification de la ligne de pourquoi ça se ferme avec ça
+        }
+
+
     }
 }
---
+++ Jeu_Poisson/UCJeu.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Jeu_Poisson
+{
+    /// <summary>
+    /// Logique d'interaction pour UCJeu.xaml
+    /// </summary>
+    public partial class UCJeu : UserControl
+    {
+        public UCJeu()
+        {
+            InitializeComponent();
+        }
+    }
+}
