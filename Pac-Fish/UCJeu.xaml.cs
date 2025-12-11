786354bf6f275aea797427fb2d326a349fc8cf29
Théo M. <theo.mauhin@etu.univ-smb.fr>
2025-12-11 09:23:01 +0100

    Récupération du code en NET 8

--- Pac-Fish/UCJeu.xaml.cs
+++ Pac-Fish/UCJeu.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Jeu_Poisson
+namespace Pac_Fish
 {
     /// <summary>
     /// Logique d'interaction pour UCJeu.xaml
